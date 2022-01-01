163f2558d9d3e0c4d8946285be7055d52610ba03
Audomaro Glez <audomaro.gonzalez@expeditors.com>
2022-01-01 00:00:00 -0600

    Inicio

---
+++ 1.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+HC() {
+    cmd="GIT_COMMITTER_DATE='$1' git commit --amend --date='$1' --no-edit"
+
+    if
+        [ -z "$1" ]
+    then
+        cmd="echo ""Se requiere la fecha para el commit"""
+    fi
+
+    eval $cmd
+}
+
+HC "2000.01.01T00:00:00"
